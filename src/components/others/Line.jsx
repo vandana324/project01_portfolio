50ec17394c91fc9136882c6428c73762a615992e
vandana__324 <vandanaramawat04@gmail.com>
2025-06-04 21:41:57 +0530

    Updates

--- src/components/others/Line.jsx
+++
@@ -1,8 +0,0 @@
-function Line(){
- return(
-    <>
-     <div className="w-24 h-1 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 mx-auto mt-4 rounded"></div>
-    </>
- )
-}
-export default Line
